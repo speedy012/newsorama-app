cc6889c872b60f60d0ab7cfea30f7bf8e4f1765c
Herman Rivas <hrivas5726@aol.com>
2020-08-21 20:59:17 -0400

    update

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -21,24 +21,23 @@ const ArticleCard = (props) => {
 
   return (
     <>
-    <button className="article-card" onClick={openModal}>
-      <div className="image">
-        <img src={props.article.urlToImage} alt="oh no!"  style={{width: '100%'}}/>
-      </div>
-      <div className="container">
-        <div className="header"> {props.article.title}</div>
-      </div>
-      <div className="extra content">
-        <button type="button" onClick={deleteArticleFromList} style={{zIndex: 2}}>Remove</button>
-      </div>
-    </button>
-    <Modal isOpen={isOpen} onRequestClose={closeModal} >
-      <h2><span>{props.article.title}</span></h2>
-      <h4>Written By: {props.article.author}</h4>
-      <p>{props.article.description}</p>
-       <a href={props.article.url}>To Read The Full Article, Click Here!</a>
-       <button className="modal-btn" onClick={closeModal}>Close</button>
-    </Modal>
+      <button className="article-card" onClick={openModal}>
+        <div className="image">
+          <img src={props.article.urlToImage} alt="oh no!"  style={{width: '100%'}}/>
+        </div>
+        <div className="container">
+          <div className="header"> {props.article.title}</div>
+        </div>
+        <div className="extra content">
+        </div>
+      </button>
+      <Modal isOpen={isOpen} onRequestClose={closeModal} >
+        <h2><span>{props.article.title}</span></h2>
+        <h4>Written By: {props.article.author}</h4>
+        <p>{props.article.description}</p>
+         <a href={props.article.url}>To Read The Full Article, Click Here!</a>
+         <button className="modal-btn" onClick={closeModal}>Close</button>
+      </Modal>
     </>
   )
 }
